7989299eda6a6dbe84d19fd7738d17bbed79c9f4
wrkconf <wrk.conf@gmail.com>
2018-01-14 21:57:23 +0400

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,15 @@
 package ru.job4j;
 
+/**
+  * Class Класс для вычисления арифметических операций + - * / ^
+  * @author Alexandr
+  * @since 14.01.2018
+  * @version 1
+ */ 
 class Calculate{
+	/**
+	* Метод main (точка входа)
+	*/
 	public static void main(String[] args){
 		System.out.println("Hello World!");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Alexandr Alexandrov
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
