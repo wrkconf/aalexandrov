21070e14e7a810b5d6c16b93ac232808550eb03a
wrkconf <wrk.conf@gmail.com>
2018-01-17 22:46:53 +0400

    Элементарный калькулятор

---
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -0,0 +1,27 @@
+package ru.job4j.condition;
+
+/**
+ * Created by Home on 17.01.2018.
+ */
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distanceTo(Point that) {
+        return Math.sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2));
+    }
+
+    public static void main(String[] args) {
+        Point a = new Point(0, 1);
+        Point b = new Point(2, 5);
+        a.distanceTo(b);
+
+        double result = a.distanceTo(b);
+        System.out.println("Расстояние между точками A и B: " + result);
+    }
+}
